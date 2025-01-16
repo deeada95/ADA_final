628cf5a57528358e7c54e44d6f6f97f6acb7bbf1
ADA <andreescu.diana@gmail.com>
2025-01-16 23:33:37 +0200

    creare modele

--- farmacie/farmacie/wsgi.py
+++ farmacie/farmacie/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pharmacy project.
+WSGI config for farmacie project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pharmacy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farmacie.settings')
 
 application = get_wsgi_application()

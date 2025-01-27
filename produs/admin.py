9c57d759249ca68b83844044d850e8fda8a1b40f
ADA <andreescu.diana@gmail.com>
2025-01-27 21:03:47 +0200

    creare modele si superuser

--- produs/admin.py
+++ produs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from produs.models import Produs, Client, Comanda
+
 # Register your models here.
+admin.site.register(Produs)
+admin.site.register(Client)
+admin.site.register(Comanda)
--- produs/models.py
+++ produs/models.py
@@ -1,4 +1,8 @@
+from datetime import datetime
 from django.db import models
+
+
+
 
 # Create your models here.
 class Produs(models.Model):
@@ -16,3 +20,21 @@ class Client(models.Model):
     adresa = models.TextField()
     def __str__(self):
         return f'{self.prenume} {self.nume}'
+
+class Comanda(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, default=1)
+    produse = models.ManyToManyField(Produs)
+    data_creare = models.DateTimeField(default=datetime.now())
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ('in asteptare', 'În așteptare'),
+            ('procesata', 'Procesată'),
+            ('livrata', 'Livrată'),
+            ('anulata', 'Anulată'),
+        ],
+        default='in asteptare',
+    )
+
+    def __str__(self):
+        return f'Comanda #{self.id} - {self.client}'
